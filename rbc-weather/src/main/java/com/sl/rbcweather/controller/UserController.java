4ce8ff147cffd49a5ad1e2f59df058c291f1cdb4
Santiago Leiva <santiagoeleiva@gmail.com>
2018-05-03 21:12:06 -0300

    Se agrega UserController.

---
+++ rbc-weather/src/main/java/com/sl/rbcweather/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.sl.rbcweather.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sl.rbcweather.model.User;
+import com.sl.rbcweather.service.UserService;
+import com.sl.rbcweather.util.RestResponse;
+
+@RestController
+@RequestMapping({"/users"})
+public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@GetMapping
+	public List<User> users() {
+		return this.userService.list();
+	}
+	
+	@PostMapping
+	public RestResponse save(@RequestBody User user) {
+		
+		if ( !user.getUsername().equals(null) && !user.getUsername().equals("") ) {
+			this.userService.save(user);
+			
+			return new RestResponse(HttpStatus.OK.value(), "The user has been saved");
+		} else {
+			return new RestResponse(HttpStatus.NOT_ACCEPTABLE.value(), "Not aceptable");
+		}
+	}
+}
